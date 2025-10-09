21aedb80753563dda3b7e24bb1c033165f2eec39
Robson Faria Matos Junior <robsonfaria@jordao.grad.dcc.ufmg.br>
2025-10-09 20:06:16 -0300

    Implementação da classe Cena (imcomp)

--- src/Cena.cpp
+++ src/Cena.cpp
@@ -1,41 +1,54 @@
 #include "../include/Cena.hpp"
-#include "../include/Trecho.hpp"
+#include "../include/Objeto.hpp"
 #include <iostream>
 
-//Construtor padrão
-Cena::Cena() {
-    tempo = -1;
-    primeiro = nullptr;
-    objetos = nullptr;
-    tamanhoObjetos = 0;
-}
+class Objeto;
 
 //Construtor
-Cena::Cena(int tempo, Objeto* objetos, int tamanhoObjetos) {
-    //Encontra o menor X que será 
-    this -> tempo = tempo;
-    this -> primeiro = nullptr;
-    this -> objetos = objetos;
-    this -> tamanhoObjetos = tamanhoObjetos;
-    int xMin = menorX(objetos, tamanhoObjetos);
+Cena::Cena(int tempo, int numObjetos, Objeto* objetos) {
+    this->tempo = tempo;
+    this->numObjetos = numObjetos;
+    primeiro = nullptr;
+
+    for(int i = 0; i < numObjetos; i++) {
+        adicionaTrecho(i, objetos);
+    }
 }
 
 //Destrutor
 Cena::~Cena() {
-    Trecho *atual = primeiro;
+    Trecho* atual = primeiro;
     while(atual != nullptr) {
-        Trecho *proximo = atual -> getProximo();
+        Trecho *proximo = atual->proximo;
         delete atual;
         atual = proximo;
     }
 }
 
-int Cena::menorX(Objeto *objetos, int tamanhoObjetos) const {
+void Cena::adicionaTrecho(int indice, Objeto* objetos) {
+    No* atual = objetos->cabeca;
+    Trecho* novoTrecho;
+    for(int i = 0; i < indice; i++) {
+        atual = atual->proximo;
+    }
 
-}
+    if(primeiro == nullptr) {
+        primeiro = novoTrecho;
+        novoTrecho->id = atual->id;
+        novoTrecho->inicio = atual->x - (atual->largura / 2);
+        novoTrecho->fim = atual->x + (atual->largura / 2);            
+        return;
+    }
 
-void Cena::adicionaTrecho() {
+    
+    //Implementar a função para definição de trecho
 
+    //Insere no fim da lista
+    Trecho* tAtual = primeiro;
+    while(tAtual->proximo != nullptr) {
+        tAtual = tAtual->proximo;
+    }
+    tAtual->proximo = novoTrecho;
 }
 
 void Cena::imprimeCena() {
--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,15 @@
 #include <sstream>
 #include <limits>
 #include "../include/Objeto.hpp"
+#include "../include/Cena.hpp"
 
 int main() {
     std::string linha;
+    int numObjs = 0;
 
     Objeto objetos;
+
+    std::cout << "Programa iniciado" << std::endl;
 
     while(std::getline(std::cin, linha)) {
         char tipo = linha[0];
@@ -20,17 +24,26 @@ int main() {
             double x, y, largura;
             iss >> id >> x >> y >> largura;
             objetos.inserir(id, x, y, largura);
+            numObjs++;
+            std::cout << "Objeto registrado" << std::endl;
         }
         if(tipo == 'M') {
             int id, tempo;
             double x, y;
             iss >> tempo >> id >> x >> y;
             objetos.movimentaObjeto(id, x, y);
+            objetos.ordenaPorY();
+            std::cout << "Comando Movimentacao registrado" << std::endl;
         }
         if(tipo == 'C') {
-
+            objetos.ordenaPorY(); //Retirar esse trecho posteriormente
+            int tempo;
+            iss >> tempo;
+            Cena(tempo, numObjs, &objetos);
+            std::cout << "Comando Cena registrado" << std::endl;
         }
 
     }
+    std::cout << "Programa encerrado" << std::endl;
     return 0;
 }
